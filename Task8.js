197061bd61b7a5fd2b731e60455ebd7d5d2347ae
ikram hussain kalhoro <kalhoroikramhussain>
2026-08-02 13:43:48 +0500

    task8 done

---
+++ Task8.js
@@ -0,0 +1,10 @@
+let marks =40;
+while (marks <=100){
+    if (marks >=50){
+        console.log(marks + "pass");
+    }
+     else {
+        console.log(marks + "Fail");
+    }
+    mAarks  +=10;
+}
