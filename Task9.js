cc6c0ef0d9b64ad8a916228154db9f7812d74d98
ikram hussain kalhoro <kalhoroikramhussain>
2026-08-02 13:49:09 +0500

    task9 done

---
+++ Task9.js
@@ -0,0 +1,11 @@
+let i=1;
+while(i<=10){
+    let result = 2 *i;
+    if(i % 2 === 0){
+        console.log(result +"even")
+    }
+     else{
+        console.log(result +"odd");
+    }
+    i++;
+}
