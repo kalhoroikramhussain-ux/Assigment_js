d0971701360a5ce8f978245235b673d2fda8cbd7
ikram hussain kalhoro <kalhoroikramhussain>
2026-08-02 13:13:13 +0500

    task 2 done

---
+++ Task_2.js
@@ -0,0 +1,12 @@
+let i=-5;
+while(i<=5){
+    if (i >0) {
+        console.log(i + " = positive");
+    } else if(i < 0) {
+        console.log(i + " = Negative");
+        }
+        else {
+        console.log(i + " = Zero");
+}
+i++;
+}
